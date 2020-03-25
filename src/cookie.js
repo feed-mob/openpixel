07bd93fc7492867a71825833b3e1ce13de5ff020
Richard Hao <ithaoxilu@gmail.com>
2020-03-25 06:19:22 +0000

    Add feedmob custom parameters supports
    https://github.com/feed-mob/feedmob/issues/7952

--- src/cookie.js
+++ src/cookie.js
@@ -65,5 +65,33 @@ var Cookie = {
       var utms = JSON.parse(this.get('utm'));
       return name in utms ? utms[name] : "";
     }
+  },
+
+  setFms() {
+    var fmArray = ['fm_click_id','fm_publisher_id','fm_conversion_id'];
+    var exists = false;
+    for (var i = 0, l = fmArray.length; i < l; i++) {
+      if (isset(Url.getParameterByName(fmArray[i]))) {
+        exists = true;
+        break;
+      }
+    }
+    if (exists) {
+      var val, save = {};
+      for (var i = 0, l = fmArray.length; i < l; i++) {
+        val = Url.getParameterByName(fmArray[i]);
+        if (isset(val)) {
+          save[fmArray[i]] = val;
+        }
+      }
+      this.set('fm', JSON.stringify(save), 2*365*24*60);
+    }
+  },
+
+  getFm(name) {
+    if (this.exists('fm')) {
+      var fms = JSON.parse(this.get('fm'));
+      return name in fms ? fms[name] : "";
+    }
   }
 }
